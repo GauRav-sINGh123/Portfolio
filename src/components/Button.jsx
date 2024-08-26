fa8d22e449b826985cca0f04866c18915c4585fe
Gaurav Singh <139119394+GauRav-sINGh123@users.noreply.github.com>
2024-08-27 00:28:42 +0530

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import "./style.css";
 function Button() {
   return (
     <>
-      <a  href="www.linkedin.com/in/gaurav-singh-746916238" target="_blank" className="ui-btn mt-3 ">
+      <a  href="https://www.linkedin.com/in/gaurav-singh-746916238/" target="_blank" className="ui-btn mt-3 ">
         <span>Let's Connect</span>
       </a>
     </>
